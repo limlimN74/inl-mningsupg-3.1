35e09139ee982dd1e84df79f4562e2e449e0a9df
lini07003 <lini07003@E-PW05C37N>
2024-11-13 12:03:08 +0100

    Add project files.

---
+++ inlämningsupg 3.1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Inlämningsupgift_3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hur gammal är du?");
+            int år = int.Parse(Console.ReadLine());
+
+            if (år >= 16 && år <= 19)
+            {
+                Console.WriteLine("Du får delta i tävlingen.");
+            }
+            else if (år < 16)
+            {
+                Console.WriteLine("Du är för ung för att delta i tävlingen.");
+            }
+            else
+            {
+                Console.WriteLine("Du är för gammal för att delta i tävlingen.");
+            }
+        }
+    }
+}
